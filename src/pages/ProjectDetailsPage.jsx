6ed681536e89ed2054b965b1310ed89bdb70953c
clara seijo <clasei@proton.me>
2024-09-25 12:28:48 +0200

    initial components and api setup done

--- src/pages/ProjectDetailsPage.jsx
+++ src/pages/ProjectDetailsPage.jsx
@@ -1,21 +1,66 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import AddTask from "../components/AddTask"; // for rendering Task Add Form
 import TaskCard from "../components/TaskCard"; // for rendering Task List
+import axios from "axios";
 
 function ProjectDetailsPage () {
+
+  // we have to call the API in every component
+  // INFO IS DYNAMIC NOT STATIC, SO WE CALLED THE API AGAIN
+  // steps: 
+  // useState, useEffect, axios , promise, loading
+  // + useParams // to extract
+
+
+  const params = useParams()
+  console.log(params)
+
+
+  // const { projectId } = useParams()
+  
+
+  const [ project, setProject ] = useState(null)
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData = async () => { // REMEMBER: invoke function in useEffect
+
+    try {
+
+      const response = await axios.get(`https://project-management-api-4641927fee65.herokuapp.com/projects/${params.projectId}?_embed=tasks`)
+      // check docs to make sure you customize the url ok
+      console.log(response)
+
+      setProject(response.data)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  if(project === null) {
+    return <h3>...loading</h3>
+  }
   
   return (
     <div className="ProjectDetailsPage">
 
       <div>
-        <h1>PROJECT_NAME</h1>
-        <p>PROJECT_DESCRIPTION</p>
+        <h1>{project.title}</h1>
+        <p>{project.description}</p>
       </div>
 
       {/* ... list of all Tasks for this Project should be rendered here */}
 
       {/* example of a single TaskCard being rendered */}
       {/* <TaskCard /> */}
+
+      {project.tasks.map((eachTask) => {
+        return ( <TaskCard key={eachTask.id} {...eachTask} /> )
+      })}
 
       {/* ... form for adding a new Task should be rendered here    */}
 
--- src/pages/ProjectListPage.jsx
+++ src/pages/ProjectListPage.jsx
@@ -1,7 +1,38 @@
 import { Link } from "react-router-dom";
 import ProjectCard from "../components/ProjectCard"; // used to render each Project
+import { useState, useEffect } from "react";
+import axios from "axios";
+// axios has been pre-isntalled with initial code --> npm i axios
+// it allows as to be anchored to the API
+
+// API can be declared globally
+// let projManagAPI = "https://project-management-api-4641927fee65.herokuapp.com/projects"
 
 function ProjectListPage() {
+  // ProjectListPage will make an external call to an API
+  // steps: 
+  // useState, useEffect, fetch / async-await, resolving promise, loading
+
+  const [ allProjects, setAllProjects ] = useState(null)
+
+  useEffect(() => {
+
+    axios.get("https://project-management-api-4641927fee65.herokuapp.com/projects")
+    // axios.get(projManagAPI) // fetch replaced with axios
+    // all data will be stored in a variable called data
+
+    // .then((response) => {
+      .then(({data}) => {
+      console.log(data)
+      // setAllProjects(response.data) // always use .data when working with axios
+      setAllProjects(data) // data destructured
+    })
+
+    .catch((err) => {
+      console.log(err)
+    })
+
+  }, [])
   
   return (
     <div className="ProjectListPage">
@@ -12,6 +43,17 @@ function ProjectListPage() {
 
       {/* ... list of all projects should be rendered here   */}
       {/* ... for each project, we should render one ProjectCard */}
+
+      {allProjects === null ? (<h3>...loading</h3>) : (
+        allProjects.map((eachProject) => {
+
+          return (
+            // <p key={eachProject.id}>{eachProject.title}</p>
+            <ProjectCard key={eachProject.id} {...eachProject}/>
+
+          )
+        })
+      )}
        
     </div>
   );
